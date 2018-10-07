b9691f62dd855ba12d1c780cc8c99d5f44b9c5ff
pepe <pepecqa.dev@gmail.com>
2018-10-07 18:03:56 -0500

    variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/tiendita';
 } else {
-    urlDB = 'mongodb://t-user:elekele1@ds125293.mlab.com:25293/tiendita';
-    // urlDB = process.env.MONGO_URI;
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
