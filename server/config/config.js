9e2a9719c68e24a8e20f02eadbe9a0f1e63d2786
pepe <pepecqa.dev@gmail.com>
2018-10-07 17:07:01 -0500

    RestServer - usuarios

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,24 @@
-// ===================================
-// Puerto
-// ===================================
+// ============================
+//  Puerto
+// ============================
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/tiendita';
+} else {
+    urlDB = 'mongodb://t-user:elekele1@ds125293.mlab.com:25293/tiendita';
+    // urlDB = process.env.MONGO_URI;
+}
+process.env.URLDB = urlDB;
